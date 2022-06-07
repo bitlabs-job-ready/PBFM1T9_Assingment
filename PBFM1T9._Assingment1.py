9e837417ab7f4fc7cd879337dbda6dc485849508
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-06-07 12:11:34 +0530

    Add files via upload

---
+++ PBFM1T9._Assingment1.py
@@ -0,0 +1,12 @@
+"""
+Write a function(preLetterCase) that accepts two parameters ( a string ,and a letter). The function must return the string with all characters before the first instance of the letter converted to lowercase and all other characters converted to uppercase. Hence, if the specified letter is the first character of the string then the entire string will be converted to uppercase. The new string should be returned by the function.
+
+e.g.
+
+preLetterCase ("CAtCHa","a") -> "cATCHA",
+preLetterCase ("Preteen","e") -> "prETEEN"
+preLetterCase ("You've got this", "m") -> "you've got this"
+preLetterCase ("Keep trying", "k") -> "KEEP TRYING"
+
+"""
+
---
+++ PBFM1T9_Assingment3.py
@@ -0,0 +1,10 @@
+"""
+
+ Write a program that create python function with the name conputePay to prompt the user for hours and rate per hour using input to compute gross pay. Use 35 hours and a rate of 2.75 per hour to test the program (the pay should be 96.25). You should use input to read a string and float() to convert the string to a number. 
+
+Sample Input:
+Enter Hours:35
+please input your rate:2.75
+Expected Output:
+ gross pay : 96.25
+"""
